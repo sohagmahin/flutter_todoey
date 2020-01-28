d6c6a937c0ba093f8fb3424062425f6f7a80a619
sohagmahin <sohagmahin@gmail.com>
2020-01-28 13:11:10 +0600

    Forced to use portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todoey/provider/task_provider.dart';
 import 'screen_task/task_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
     return ChangeNotifierProvider<TaskProvider>(
       create: (context)=> TaskProvider(),
       child: MaterialApp(
