f92615b2b5a3fb5b363e6e469b88b26d036b402f
sohagmahin <sohagmahin@gmail.com>
2020-02-02 19:21:55 +0600

    added exitDialog

--- lib/screen_task/task_screen.dart
+++ lib/screen_task/task_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_todoey/widgets/task_list.dart';
 import 'package:flutter_todoey/screen_task/addtask_screen.dart';
@@ -6,73 +7,93 @@ import 'package:provider/provider.dart';
 import '../widgets/my_drawer.dart';
 
 class TaskScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: MyDrawer(),
-      backgroundColor: Theme.of(context).primaryColor,
-      floatingActionButton: FloatingActionButton(
+    return WillPopScope(
+      onWillPop: () => exitDialog(context),
+      child: Scaffold(
+        drawer: MyDrawer(),
         backgroundColor: Theme.of(context).primaryColor,
-        splashColor: Theme.of(context).splashColor,
-        child: Icon(
-          Icons.add,
-          size: 35.0,
-          color: Colors.white,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Theme.of(context).primaryColor,
+          splashColor: Theme.of(context).splashColor,
+          child: Icon(
+            Icons.add,
+            size: 35.0,
+            color: Colors.white,
+          ),
+          onPressed: () => showModalBottomSheet(
+            context: context,
+            builder: (context) => AddTask(),
+          ),
         ),
-        onPressed: () => showModalBottomSheet(
-          context: context,
-          builder: (context) => AddTask(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                  top: 60.0, left: 30.0, right: 30.0, bottom: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildCustomMenuButton(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '${Provider.of<MainModel>(context).taskCount} tasks',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Remain: ${Provider.of<MainModel>(context).remainCount}',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: TaskList(),
+            )
+          ],
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-                top: 60.0, left: 30.0, right: 30.0, bottom: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildCustomMenuButton(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'Todoey',
-                  style: TextStyle(
-                      fontSize: 35.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      '${Provider.of<MainModel>(context).taskCount} tasks',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'Remain: ${Provider.of<MainModel>(context).remainCount}',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-              ],
-            ),
+    );
+  }
+  Future<bool> exitDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Do you want to exit'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('No'),
+            onPressed: () => Navigator.of(context).pop(),
           ),
-          Expanded(
-            child: TaskList(),
-          )
+          FlatButton(
+            child: Text('Yes'),
+            onPressed: () => exit(0),
+          ),
         ],
       ),
     );
@@ -83,30 +104,31 @@ class _buildCustomMenuButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.white30,
-                blurRadius: 2,
-                spreadRadius: 5,
-                offset: Offset(0.5, 1))
-          ],
-        ),
-        child: GestureDetector(
-          onTap: () {
-            print('Tapped');
-            Scaffold.of(context).openDrawer();
-          },
-          child: CircleAvatar(
-            radius: 35.0,
-            backgroundColor: Theme.of(context).primaryColor,
-            child: Icon(
-              Icons.list,
-              size: 40.0,
-              color: Theme.of(context).accentColor,
-            ),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.white30,
+              blurRadius: 2,
+              spreadRadius: 5,
+              offset: Offset(0.5, 1))
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          print('Tapped');
+          Scaffold.of(context).openDrawer();
+        },
+        child: CircleAvatar(
+          radius: 35.0,
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Icon(
+            Icons.list,
+            size: 40.0,
+            color: Theme.of(context).accentColor,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
